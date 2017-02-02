9047ec619c78ff8ce54a161424b5cc76e9c0cacb
mania4u <hs1942@nyu.edu>
2017-02-02 14:42:38 -0500

    add time calculation

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,10 +30,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 
 clf = GaussianNB()
-clf.fit(features_train, labels_train)
-pred = clf.predict(features_test)
 
-print clf.score(features_test, labels_test)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time() - t1, 3), "s"
+
+print "The accuracy of my Naive Bayes author identifier is", clf.score(features_test, labels_test)
 
 #########################################################
 
