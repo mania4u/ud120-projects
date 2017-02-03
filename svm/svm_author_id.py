e1959aeb55e476d1793dfcc8dd0060a39d4e50b7
mania4u <hs1942@nyu.edu>
2017-02-03 12:13:32 -0500

    Answer for SVM Mini Project
    the pc only alllows me to use 1% samples or it won't give me results

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(kernel="rbf", C=10000)
+
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+clf.fit(features_train, labels_train)
+pred = clf.predict([features_test[10]])
+
+print accuracy_score(pred, labels_test)
+
 #########################################################
 
 
